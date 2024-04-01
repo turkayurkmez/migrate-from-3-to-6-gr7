efebe20b0e1e04a8b648390e168c6ac35e688dc8
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-04-01 12:00:11 +0300

    Natural types in lambda expression

--- ChangesInCSharp/ChangesInCSharp/Program.cs
+++ ChangesInCSharp/ChangesInCSharp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+#region global usings, implicitly using ve file-scoped namespace
+
+
+void getHello() => Console.WriteLine("Metottan selam!");
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+getHello();
+
+DataTable dataTable = new DataTable();
+
+#endregion
+
+#region Lambda ve anonim tiplerde "doğal" yetenek
+//eski:
+Func<int, bool> isEven = number => number % 2 == 0;
+//yeni:
+var isEvenAnonym = (int number) => number % 2 == 0;
+
+IEnumerable<int> numbers = Enumerable.Range(1, 10);
+numbers.Where(isEvenAnonym);
+//var isOddAnonym = number => number % 2 == 1;...... anonim tipin anlaşılması parametre tipi mutlaka belirli olmalı.
+Delegate isOddAnonym = (int number) => number % 2 == 1;
+
+//eski:
+Func<int> read = Console.Read;
+Action<string> write = Console.Write;
+//yeni
+var read2 = Console.Read;
+//var write2 = Console.Write -- Çalışmaz!;
+
+//lambda expression'lara dönüş değeri belirtileviliyor:
+var result = object (bool isCancelled) => isCancelled ? "iptal edildi" : -1;
+
+#endregion
+
